0ca96acdeb64dc88c9e1840f9edec5b670f6f57b
balmerd <david.b.balmer@gmail.com>
2014-05-29 00:45:22 -0700

    Replaced task.sequence with $index

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,7 @@
 var taskApp = angular.module('taskApp', ['uuid4', 'ngStorage']);
 
 // include string names for the arguments eg: '$scope' so that they are not replaced when generating minified or obfuscated javascript
-taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', function ($scope, $log, $localStorage, uuid4) {
+taskApp.controller('taskController', ['$scope', '$log', '$localStorage', 'uuid4', function ($scope, $log, $localStorage, uuid4) {
 
     // DATA
     ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -22,28 +22,28 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
         list: ['todo', 'other'] // these show up in the dropdown
     };
 
-    //$scope.$storage.todo = [
-    //    {
-    //        id: uuid4.generate(),
-    //        sequence: 0,
-    //        name: 'California',
-    //        tasks: [
-    //            { id: uuid4.generate(), sequence: 0, name: 'Hiking', details: 'on Mt. Tamalpais' },
-    //            { id: uuid4.generate(), sequence: 1, name: 'Camping', details: 'in Carson Pass' },
-    //            { id: uuid4.generate(), sequence: 2, name: 'Kayaking', details: 'in San Francisco Bay' }
-    //        ]
-    //    },
-    //    {
-    //        id: uuid4.generate(),
-    //        sequence: 1,
-    //        name: 'Toronto',
-    //        tasks: [
-    //            { id: uuid4.generate(), sequence: 0, name: 'Fishing', details: 'at the Cottage' },
-    //            { id: uuid4.generate(), sequence: 1, name: 'Biking', details: 'at the Island' },
-    //            { id: uuid4.generate(), sequence: 2, name: 'Visiting', details: 'with Marcel, Rhiane, Charis, Ashley, Philip, Kizzy, Caleb, the newbie, Laurel, Dan, Marley, Mom, Dad, Auntie Jo, Mr. Vetere, Gus, Patrick, Brian' }
-    //        ]
-    //    }
-    //];
+    $scope.$storage.todo = [
+        {
+            id: uuid4.generate(),
+            sequence: 0,
+            name: 'California',
+            tasks: [
+                { id: uuid4.generate(), name: 'Hiking', details: 'on Mt. Tamalpais' },
+                { id: uuid4.generate(), name: 'Camping', details: 'in Carson Pass' },
+                { id: uuid4.generate(), name: 'Kayaking', details: 'in San Francisco Bay' }
+            ]
+        },
+        {
+            id: uuid4.generate(),
+            sequence: 1,
+            name: 'Toronto',
+            tasks: [
+                { id: uuid4.generate(), name: 'Fishing', details: 'at the Cottage' },
+                { id: uuid4.generate(), name: 'Biking', details: 'at the Island' },
+                { id: uuid4.generate(), name: 'Visiting', details: 'with Marcel, Rhiane, Charis, Ashley, Philip, Kizzy, Caleb, the newbie, Laurel, Dan, Marley, Mom, Dad, Auntie Jo, Mr. Vetere, Gus, Patrick, Brian' }
+            ]
+        }
+    ];
 
     $scope.categories = $scope.$storage.todo;
 
@@ -77,53 +77,74 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
             group: 'tasks',
             draggable: '.list-group-item',
             handle: '.item-handle',
-            onAdd: function (evt) { // fired after remove in no-jquery-sortable.js
+            onAdd: function (evt) { // fired after onRemove() in sortable.js
                 Sortable.utils.stopPropagation(evt);
+                //$scope.$apply(function () {
+                    var next = evt.item.nextElementSibling;
+                    var prev = evt.item.previousElementSibling;
+                    var nextIndex = next ? next.getAttribute('data-index') : null;
+                    var prevIndex = prev ? prev.getAttribute('data-index') : null;
+                    var newIndex;
 
-                $scope.$apply(function () {
                     getCurrentCategoryAndTask(evt, listElement);
 
+                    // TODO: can move to first or last, but not middle -- everything after newIndex is gone, but still in $scope.currentCategory.tasks
+
+                    if (prev && next) { // when we have both, nextIndex is always > prevIndex
+                        newIndex = nextIndex;
+                        $log.warn(sprintf('(2) splice at %s', nextIndex));
+                    } else if (next) {
+                        newIndex = nextIndex;
+                        $log.warn(sprintf('(2) splice at %s', nextIndex));
+                    } else if (prev) {
+                        newIndex = prevIndex;
+                        $log.warn(sprintf('(2) splice at %s', prevIndex));
+                    }
+
                     // add task to our scope
-
-                    $scope.currentCategory.tasks.push($scope.currentTask);
-
-                    // reorder task.sequence by LI order (orderBy doesn't work if "track by" is specified)
-
-                    angular.forEach(evt.item.parentNode.getElementsByTagName('li'), function (item, index) {
-                        _.find($scope.currentCategory.tasks, function (task) { return task.id === item.id; }).sequence = index;
-                    });
-
-                    // remove dropped LI from the UL, otherwise we get a duplicate when angular re-renders the list -- why???
-
-                    evt.item.parentNode.removeChild(evt.item);
-                });
+                    $log.debug($scope.currentCategory.tasks.length + ' tasks BEFORE');
+                    $scope.currentCategory.tasks.splice(newIndex, 0, $scope.currentTask);
+                    $log.debug($scope.currentCategory.tasks.length + ' tasks AFTER');
+                //});
             },
             onUpdate: function (evt) {
                 Sortable.utils.stopPropagation(evt);
 
                 $scope.$apply(function () {
+                    var next = evt.item.nextElementSibling;
+                    var prev = evt.item.previousElementSibling;
+                    var nextIndex = next ? next.getAttribute('data-index') : null;
+                    var prevIndex = prev ? prev.getAttribute('data-index') : null;
+                    var currentIndex = evt.item.getAttribute('data-index');
+                    var newIndex;
+
                     getCurrentCategoryAndTask(evt, listElement);
 
-                    // reorder task.sequence by LI order (orderBy doesn't work if "track by" is specified)
+                    if (prev && next) { // when we have both, nextIndex is always > prevIndex
+                        if (nextIndex > currentIndex) {
+                            newIndex = prevIndex;
+                            //$log.warn(sprintf('(1) moving DOWN from %s to %s', currentIndex, prevIndex));  // 0-1
+                        } else {
+                            newIndex = nextIndex;
+                            //$log.warn(sprintf('(1) moving UP from %s to %s', currentIndex, nextIndex));  // 2-1
+                        }
+                    } else if (next) {
+                        newIndex = nextIndex;
+                        //$log.warn(sprintf('(2) moving UP from %s to %s', currentIndex, nextIndex));        // 1-0
+                    } else if (prev) {
+                        newIndex = prevIndex;
+                        //$log.warn(sprintf('(2) moving DOWN from %s to %s', currentIndex, prevIndex));      // 1-2
+                    }
 
-                    angular.forEach(evt.item.parentNode.getElementsByTagName('li'), function (item, index) {
-                        _.find($scope.currentCategory.tasks, function (task) { return task.id === item.id; }).sequence = index;
-                    });
-
+                    // move 1 item at $scope.currentCategory.tasks[currentIndex] to [newIndex]
+                    $scope.currentCategory.tasks.splice(newIndex, 0, $scope.currentCategory.tasks.splice(currentIndex, 1)[0]);
                 });
             },
-            onRemove: function (evt) { // fired before add in no-jquery-sortable.js
+            onRemove: function (evt) { // fired before onAdd() in sortable.js
                 Sortable.utils.stopPropagation(evt);
-
-                getCurrentCategoryAndTask(evt, listElement);
-
+                getCurrentCategoryAndTask(evt, listElement); // capture current task for use in onAdd()
+                // remove 1 item at $scope.currentCategory.tasks[index]
                 $scope.currentCategory.tasks.splice($scope.currentCategory.tasks.indexOf($scope.currentTask), 1);
-
-                // reorder task.sequence (orderBy doesn't work if "track by" is specified)
-
-                angular.forEach($scope.currentCategory.tasks, function (task, index) {
-                    task.sequence = index;
-                });
             }
         });
     }
@@ -148,15 +169,15 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
         setTimeout(function () { // wait for render
             createSortable(multi.getElementsByClassName('list-group')[categoryIndex]);
 
-            category.removeWatchHandler = $scope.$watch('categories[' + categoryIndex + '].tasks', function (newValues, oldValues) {
-                if (newValues !== oldValues) {
-                    $log.warn(category.name + ' changed');
-                    angular.forEach(category.tasks, function (task) {
-                        $log.info(sprintf('%s(%s)', task.name, task.sequence));
-                    });
-                    // TODO: update persistent store here
-                }
-            }, true);
+            //category.removeWatchHandler = $scope.$watch('categories[' + categoryIndex + '].tasks', function (newValues, oldValues) {
+            //    if (newValues !== oldValues) {
+            //        $log.warn(category.name + ' changed');
+            //        angular.forEach(category.tasks, function (task) {
+            //            $log.info(sprintf('%s(%s)', task.name, task.sequence));
+            //        });
+            //        // TODO: update persistent store here
+            //    }
+            //}, true);
         }, 500)
     };
 
@@ -191,7 +212,7 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
             category.removeWatchHandler();
         }
 
-        $scope.categories.splice(index, 1);
+        $scope.categories.splice(index, 1); // remove 1 item at categories[index]
     };
 
     $scope.removeCollection = function (collection) {
@@ -213,17 +234,37 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     angular.forEach($scope.categories, function (category, categoryIndex) {
-        category.removeWatchHandler = $scope.$watch('categories[' + categoryIndex + '].tasks', function (newValues, oldValues) { // watch for task changes
+        //category.removeWatchHandler = $scope.$watch('categories[' + categoryIndex + '].tasks', function (newValues, oldValues) { // watch for task changes
+        //    if (newValues !== oldValues) {
+        //        $log.warn(category.name + ' changed');
+        //        angular.forEach(category.tasks, function (task) {
+        //            $log.info(sprintf('%s(%s)', task.name, task.sequence));
+        //        });
+        //        // update persistent store
+        //        var categoryIndex = $scope.$storage.todo.indexOf(category);
+        //        $scope.$storage.todo[categoryIndex] = category;
+        //    }
+        //}, true);
+        //$scope.$watch('categories.length', function (newValues, oldValues) { // watch for changes
+        //    if (newValues !== oldValues) {
+        //        $log.warn('category.length change watched');
+        //    }
+        //});
+        //$scope.$watch('categories[' + categoryIndex + '].sequence', function (newValues, oldValues) { // watch for changes
+        //    if (newValues !== oldValues) {
+        //        $log.warn('category.sequence change watched');
+        //    }
+        //});
+        //$scope.$watch('categories[' + categoryIndex + '].tasks.length', function (newValues, oldValues) { // watch for changes
+        //    if (newValues !== oldValues) {
+        //        $log.warn('category.tasks.length change watched');
+        //    }
+        //});
+        $scope.$watchCollection('categories[' + categoryIndex + '].tasks', function (newValues, oldValues) { // watch for changes
             if (newValues !== oldValues) {
-                $log.warn(category.name + ' changed');
-                angular.forEach(category.tasks, function (task) {
-                    $log.info(sprintf('%s(%s)', task.name, task.sequence));
-                });
-                // update persistent store
-                var categoryIndex = $scope.$storage.todo.categories.indexOf(category);
-                $scope.$storage.todo.categories[categoryIndex] = category;
+                $log.warn('category.tasks change watched');
             }
-        }, true);
+        });
     });
 
     setTimeout(function () { // wait until list is rendered
@@ -231,16 +272,28 @@ taskApp.controller('TaskListCtrl', ['$scope', '$log', '$localStorage', 'uuid4', 
             draggable: '.tile',
             handle: '.glyph-link-move',
             onAdd: function (evt) {
-                $log.debug('category added');
                 Sortable.utils.stopPropagation(evt);
+                $log.debug('category added');
             },
             onUpdate: function (evt) {
-                $log.debug('category updated');
                 Sortable.utils.stopPropagation(evt);
+
+                $scope.$apply(function () {
+                    $log.debug('category updated');
+
+                    // TODO: reorder categories
+                    angular.forEach(evt.item.parentNode.getElementsByClassName('tile'), function (item, index) {
+                        var obj = _.find($scope.categories, function (cat) {
+                            return cat.id === item.id;
+                        });
+                        obj.sequence = index;
+                        console.log('set ' + obj.name + '.sequence=' + index);
+                    });
+                });
             },
             onRemove: function (evt) {
-                $log.debug('category removed');
                 Sortable.utils.stopPropagation(evt);
+                $log.debug('category removed');
             }
         });
 
